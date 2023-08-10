df3445f4cf79a16f7c30746b47dee2e87b98d616
Felix Rödel <felix.roedel@woehner.de>
2023-08-10 12:48:20 +0200

    initial commit

---
+++ gui.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+# Create a main window
+root = tk.Tk()
+
+# Create a label widget
+label = tk.Label(root, text="Hello, Tkinter!")
+
+# Pack the label widget to display it in the window
+label.pack()
+
+# Start the main loop
+root.mainloop()
---
+++ main.py
@@ -0,0 +1,9 @@
+from threading import Thread
+
+def check_for_typing_error():
+
+
+
+
+if __name__ == "__main__":
+    Thread(target=check_for_typing_error()).start()
